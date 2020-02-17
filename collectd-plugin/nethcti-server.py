021135f57faa1c91014ee3989a75127033edc632
Alessandro Polidori <alessandro.polidori@nethesis.it>
2020-02-17 12:02:55 +0100

    collectd: add number of calls

--- collectd-plugin/nethcti-server.py
+++ collectd-plugin/nethcti-server.py
@@ -1,14 +1,20 @@
 import json
 import collectd
 import requests
-import random
+import MySQLdb
+import datetime
 
 CONFIG = {
     'Host': '127.0.0.1',
-    'Debug' : 'False'
+    'Debug': 'False',
+    'DB_Host': '127.0.0.1',
+    'DB_User': '',
+    'DB_Password': '',
+    'DB_Name': 'asteriskcdrdb'
 }
 REST_PROF = 'http://127.0.0.1/webrest/profiling/all'
 PLUGIN_NAME = 'nethcti-server'
+QUERY=""
 
 def clog(msg):
     if CONFIG['Debug'] == 'True':
@@ -19,13 +25,17 @@ def config_cb(conf):
         CONFIG[node.key] = node.values[0]
 
 def init_cb():
+    global QUERY
     clog('init')
+    now = datetime.datetime.now()
+    delta = datetime.timedelta(minutes=1)
+    earlier = (now - delta).strftime("%Y-%m-%d %H:%M:00")
+    QUERY="SELECT count(*) FROM cdr WHERE calldate >= \"" + earlier + "\" AND calldate < \"" + now.strftime("%Y-%m-%d %H:%M:00") + "\""
 
 def read_cb():
     clog('GET ' + REST_PROF)
     r = requests.get(REST_PROF)
     info = json.loads(r.text)
-    print("info")
     if r.status_code == 200:
         # users
         tot_users = info['tot_users']
@@ -45,6 +55,18 @@ def read_cb():
         dispatch_value('Memory usage', 'External', external, 'memory')
     else:
         clog('error GET ' + REST_PROF + ': status code ' + str(r.status_code))
+    # number of calls
+    try:
+        clog(CONFIG['Host'])
+        db = MySQLdb.connect(host=CONFIG['DB_Host'], user=CONFIG['DB_User'], passwd=CONFIG['DB_Password'], db=CONFIG['DB_Name'])
+        cur = db.cursor()
+        cur.execute(QUERY)
+        count=cur.fetchone()[0]
+        dispatch_value('Num Calls', 'Num Calls', count, 'gauge')
+        db.close()
+    except Exception as err:
+        clog('error mysql ' + QUERY)
+        clog(err)
 
 def dispatch_value(plugin_instance, type_instance, value, type):
     clog("sending value: %s / %s = %s" % (plugin_instance, type_instance, value))
@@ -56,9 +78,6 @@ def dispatch_value(plugin_instance, type_instance, value, type):
     vl.values = [value]
     vl.dispatch()
     
-
-
-
 collectd.register_config(config_cb)
 collectd.register_init(init_cb)
 collectd.register_read(read_cb)
